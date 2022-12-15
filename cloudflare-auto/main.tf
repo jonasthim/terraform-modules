515c1eb666f965502916c240a06c106f06c325e8
Jonas Thim <jonas.thim@gmail.com>
2022-12-15 18:54:05 +0100

    first commit

---
+++ cloudflare-auto/main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+output "test" {
+  value = var.cloudflare_api_token
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+data "cloudflare_zone" "domain" {
+  name = var.domain.name
+}
+
+resource "cloudflare_record" "domain" {
+  zone_id         = data.cloudflare_zone.domain.zone_id
+  name            = var.domain.name
+  value           = var.domain.target
+  type            = "A"
+  ttl             = var.default_ttl
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "dns" {
+  for_each = {
+    for index, record in var.dns_records :
+    record.name => record
+    if record.protected != true || record.local-port != ""
+  }
+  zone_id         = data.cloudflare_zone.domain.zone_id
+  name            = each.value.name
+  value           = each.value.value
+  type            = each.value.type
+  ttl             = each.value.proxied ? 1 : var.default_ttl
+  proxied         = each.value.proxied
+  allow_overwrite = true
+}
+
+resource "cloudflare_access_application" "cf_app" {
+  for_each = {
+    for index, record in var.dns_records : index => record
+    if record.protected == true
+  }
+  zone_id          = data.cloudflare_zone.domain.zone_id
+  name             = each.value.name
+  domain           = "${each.value.name}.${var.domain.name}"
+  session_duration = "1h"
+}
+
+resource "cloudflare_access_policy" "policy" {
+  for_each       = cloudflare_access_application.cf_app
+  application_id = cloudflare_access_application.cf_app[each.key].id
+  zone_id        = data.cloudflare_zone.domain.zone_id
+  name           = "Allowed e-mailaddresses for ${var.dns_records[each.key].name}"
+  precedence     = "1"
+  decision       = "allow"
+
+  include {
+    email = var.dns_records[each.key].allowed_emails
+  }
+}
+
+resource "cloudflare_argo_tunnel" "home" {
+  account_id = data.cloudflare_zone.domain.account_id
+  name       = "Home"
+  secret     = var.tunnel_secret
+}
+
+output "key" {
+  value     = cloudflare_argo_tunnel.home.tunnel_token
+  sensitive = true
+}
+
+resource "cloudflare_tunnel_config" "tunnel" {
+  account_id = data.cloudflare_zone.domain.account_id
+  tunnel_id  = cloudflare_argo_tunnel.home.id
+  config {
+    dynamic "ingress_rule" {
+      for_each = {
+        for index, record in var.dns_records :
+        record.name => record
+        if record.protected == true && record.local-port != ""
+      }
+      content {
+        hostname = "${ingress_rule.value.name}.${var.domain.name}"
+        service  = "${ingress_rule.value.local-tls ? "https://" : "http://"}${ingress_rule.value.name}:${ingress_rule.value.local-port}"
+      }
+    }
+    ingress_rule {
+      service = "https://idontexist"
+    }
+  }
+
+}
---
+++ cloudflare-auto/variables.tf
@@ -0,0 +1,56 @@
+variable "dns_records" {
+  description = "Value of proxied DNS records"
+  type = list(object({
+    name       = string
+    type       = string
+    value      = string
+    protected  = bool
+    proxied    = bool
+    local-port = string
+    local-tls  = bool
+    allowed_emails = list(string)
+  }))
+  default = [{ "name" : "", "type" : "", "value" : "", "protected" : false, "proxied" : true, "local-port" : "", "local-tls" : false, "allowed_emails": [""] }]
+}
+
+variable "cloudflare_api_token" {
+  description = "API token for cloudflare"
+  type        = string
+  default     = ""
+  sensitive = true
+}
+
+variable "tunnel_secret" {
+  description = "API token for cloudflare"
+  type        = string
+  default     = ""
+  sensitive = true
+  validation {
+    condition     = length(var.tunnel_secret) == 184
+    error_message = "You must use a 184 length secret"
+  }
+}
+
+variable "domain" {
+  description = "Domain that you want to modify"
+  type = object({
+    name   = string
+    target = string
+  })
+  default = {
+    name   = ""
+    target = ""
+  }
+}
+
+variable "allowed_emails" {
+  description = "A list of e-mailaddresses that can login via Cloudflare Access"
+  type        = list(string)
+  default     = [""]
+}
+
+variable "default_ttl" {
+  description = "Default TTL"
+  type        = number
+  default     = 3600
+}
