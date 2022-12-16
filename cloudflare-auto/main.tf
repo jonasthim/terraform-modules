5399d9281fe59e784f65f62dbd6635e51feb712f
Jonas Thim <jonas.thim@gmail.com>
2022-12-16 19:48:05 +0100

    Remove faulty block

--- cloudflare-auto/main.tf
+++ cloudflare-auto/main.tf
@@ -27,13 +27,11 @@ resource "cloudflare_record" "domain" {
 
 resource "cloudflare_record" "dns" {
   for_each = {
-    for record in var.dns_records : record => {
-      for item in record :
+    for key, value in var.dns_records : record => {
+      for key in value :
         item => record
-      }
-      if record["zero_trust"] == null
     }
- 
+    if record["zero_trust"] == null
   }
   zone_id         = data.cloudflare_zone.domain.zone_id
   name            = each.value.name
