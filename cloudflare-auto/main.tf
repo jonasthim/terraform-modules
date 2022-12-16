d1a682462ce9a38b16e4c046ae1d0f6142a8d060
Jonas Thim <jonas.thim@gmail.com>
2022-12-16 19:05:55 +0100

    Remove faulty block

--- cloudflare-auto/main.tf
+++ cloudflare-auto/main.tf
@@ -28,7 +28,7 @@ resource "cloudflare_record" "domain" {
 resource "cloudflare_record" "dns" {
   for_each = {
     for index, record in var.dns_records : record.name => record
-    if record.zero_trust == null && (record.zero_trust != null ? record.zero_trust.tunnel == null ? true : false : false)
+    if record.zero_trust == null || (record.zero_trust != null ? record.zero_trust.tunnel == null ? true : false : false)
   }
   zone_id         = data.cloudflare_zone.domain.zone_id
   name            = each.value.name 
@@ -39,71 +39,67 @@ resource "cloudflare_record" "dns" {
   allow_overwrite = true
 }
 
-resource "cloudflare_access_application" "cf_app" {
-  for_each = {
-    for index, record in var.dns_records : record.name => record
-    if record.zero_trust != null ? record.zero_trust.protected != null : false
-  }
-  zone_id          = data.cloudflare_zone.domain.zone_id
-  name             = title(each.value.name)
-  domain           = "${each.value.name}.${var.domain.name}"
-  session_duration = "1h"
-  allowed_idps = each.value.zero_trust.allowed_idps == null ? var.default_allowed_idps : each.value.zero_trust.allowed_idps
-  auto_redirect_to_identity = true
-}
+# resource "cloudflare_access_application" "cf_app" {
+#   for_each = {
+#     for index, record in var.dns_records : record.name => record
+#     if record.zero_trust != null ? record.zero_trust.protected != null : false
+#   }
+#   zone_id          = data.cloudflare_zone.domain.zone_id
+#   name             = title(each.value.name)
+#   domain           = "${each.value.name}.${var.domain.name}"
+#   session_duration = "1h"
+#   allowed_idps = each.value.zero_trust.allowed_idps == null ? var.default_allowed_idps : each.value.zero_trust.allowed_idps
+#   auto_redirect_to_identity = true
+# }
 
-resource "cloudflare_access_policy" "policy" {
-  for_each = {
-    for index, record in var.dns_records : record.name => record
-    if record.zero_trust != null ? record.zero_trust.protected != null : false
-  }
-  application_id = cloudflare_access_application.cf_app[each.value.name].id
-  zone_id        = data.cloudflare_zone.domain.zone_id
-  name           = "Allowed e-mailaddresses"
-  precedence     = "1"
-  decision       = "allow"
-  include {
-    email = each.value.zero_trust.allowed_emails == null ? var.default_allowed_emails : each.value.zero_trust.allowed_emails
-  }
-}
+# resource "cloudflare_access_policy" "policy" {
+#   for_each = {
+#     for index, record in var.dns_records : record.name => record
+#     if record.zero_trust != null ? record.zero_trust.protected != null : false
+#   }
+#   application_id = cloudflare_access_application.cf_app[each.value.name].id
+#   zone_id        = data.cloudflare_zone.domain.zone_id
+#   name           = "Allowed e-mailaddresses"
+#   precedence     = "1"
+#   decision       = "allow"
+#   include {
+#     email = each.value.zero_trust.allowed_emails == null ? var.default_allowed_emails : each.value.zero_trust.allowed_emails
+#   }
+# }
 
-resource "cloudflare_argo_tunnel" "default" {
-  for_each = {
-    for index, tunnel in compact(concat([var.default_tunnel_name], [for record in var.dns_records: record.zero_trust != null ? record.zero_trust.tunnel != null ? record.zero_trust.tunnel.name : "" : "" ])) : tunnel => tunnel
-  }
-  account_id = data.cloudflare_zone.domain.account_id
-  name       = each.value
-  secret     = var.tunnel_secret
-}
+# resource "cloudflare_argo_tunnel" "default" {
+#   for_each = {
+#     for index, tunnel in compact(concat([var.default_tunnel_name], [for record in var.dns_records: record.zero_trust != null ? record.zero_trust.tunnel != null ? record.zero_trust.tunnel.name : "" : "" ])) : tunnel => tunnel
+#   }
+#   account_id = data.cloudflare_zone.domain.account_id
+#   name       = each.value
+#   secret     = var.tunnel_secret
+# }
 
-resource "cloudflare_tunnel_config" "tunnel" {
-  for_each   = cloudflare_argo_tunnel.default
-  account_id = data.cloudflare_zone.domain.account_id
-  tunnel_id  = cloudflare_argo_tunnel.default[each.key].id
-  config {
-    warp_routing {
-      enabled = false
-    }
-    dynamic "ingress_rule" {
-      for_each = {
-        for index, record in var.dns_records :
-        record.name => record
-        if record.zero_trust != null ? record.zero_trust.protected == true ? record.zero_trust.tunnel != null ? record.zero_trust.tunnel.local-port != null : false : false : false
-      }
-      content {
-        hostname = "${ingress_rule.value.name}.${var.domain.name}"
-        service  = "${ingress_rule.value.zero_trust.tunnel.local-protocol == null ? "http" : ingress_rule.value.zero_trust.tunnel.local-protocol}://${ingress_rule.value.zero_trust.tunnel.local-ip != null ? ingress_rule.value.zero_trust.tunnel.local-ip : ingress_rule.value.name}:${ingress_rule.value.zero_trust.tunnel.local-port}"
-      }
-    }
-    ingress_rule {
-      service = "https://idontexist"
-    }
-  }
-}
-
-output "test" {
-  value = cloudflare_tunnel_config.tunnel
-}
+# resource "cloudflare_tunnel_config" "tunnel" {
+#   for_each   = cloudflare_argo_tunnel.default
+#   account_id = data.cloudflare_zone.domain.account_id
+#   tunnel_id  = cloudflare_argo_tunnel.default[each.key].id
+#   config {
+#     warp_routing {
+#       enabled = false
+#     }
+#     dynamic "ingress_rule" {
+#       for_each = {
+#         for index, record in var.dns_records :
+#         record.name => record
+#         if record.zero_trust != null ? record.zero_trust.protected == true ? record.zero_trust.tunnel != null ? record.zero_trust.tunnel.local-port != null : false : false : false
+#       }
+#       content {
+#         hostname = "${ingress_rule.value.name}.${var.domain.name}"
+#         service  = "${ingress_rule.value.zero_trust.tunnel.local-protocol == null ? "http" : ingress_rule.value.zero_trust.tunnel.local-protocol}://${ingress_rule.value.zero_trust.tunnel.local-ip != null ? ingress_rule.value.zero_trust.tunnel.local-ip : ingress_rule.value.name}:${ingress_rule.value.zero_trust.tunnel.local-port}"
+#       }
+#     }
+#     ingress_rule {
+#       service = "https://idontexist"
+#     }
+#   }
+# }
 
 # resource "cloudflare_record" "dns-tunnel" {
 #   for_each = {
