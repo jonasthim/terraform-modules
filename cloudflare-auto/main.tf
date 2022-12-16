142c56362adda231f9b71ff81dd57c4aa7e2229d
Jonas Thim <jonas.thim@gmail.com>
2022-12-16 15:21:42 +0100

    .

--- cloudflare-auto/main.tf
+++ cloudflare-auto/main.tf
@@ -24,7 +24,7 @@ resource "cloudflare_record" "domain" {
   value           = var.domain.target
   type            = "A"
   ttl             = 1
-  proxied         = true
+  proxied         = var.domain.proxied ? 1 : var.domain.ttl
   allow_overwrite = true
 }
 
@@ -32,12 +32,12 @@ resource "cloudflare_record" "dns" {
   for_each = {
     for index, record in var.dns_records :
     record.name => record
-    if record.protected != true || record.protocol == ""
+    if record.zero_trust == null || record.zero_trust.tunnel == null
   }
   zone_id         = data.cloudflare_zone.domain.zone_id
-  name            = each.value.name
-  value           = each.value.value
-  type            = each.value.type
+  name            = each.value.name 
+  value           = each.value.value == null ? var.default_domain : each.value.value
+  type            = each.value.type == null ? var.default_dns_type : each.value.type
   ttl             = each.value.proxied ? 1 : var.default_ttl
   proxied         = each.value.proxied
   allow_overwrite = true
@@ -46,7 +46,7 @@ resource "cloudflare_record" "dns" {
 resource "cloudflare_access_application" "cf_app" {
   for_each = {
     for index, record in var.dns_records : index => record
-    if record.protected == true
+    if record.zero_trust != null
   }
   zone_id          = data.cloudflare_zone.domain.zone_id
   name             = title(each.value.name)
@@ -60,24 +60,19 @@ resource "cloudflare_access_policy" "policy" {
   for_each       = cloudflare_access_application.cf_app
   application_id = cloudflare_access_application.cf_app[each.key].id
   zone_id        = data.cloudflare_zone.domain.zone_id
-  name           = "Allowed e-mailaddresses for ${var.dns_records[each.key].name}"
+  name           = "Allowed e-mailaddresses for ${var.dns_records[each.key].allowed_emails == null ? var.default_allowed_emails : var.dns_records[each.key].allowed_emails}"
   precedence     = "1"
   decision       = "allow"
-  
   include {
-    email = var.dns_records[each.key].allowed_emails
+    email = var.dns_records[each.key].allowed_emails == null ? var.default_allowed_emails : var.dns_records[each.key].allowed_emails
   }
 }
 
 resource "cloudflare_argo_tunnel" "home" {
+  count      = var.default_tunnel_name ? 1 : len
   account_id = data.cloudflare_zone.domain.account_id
-  name       = "Home"
+  name       = var.default_tunnel_name
   secret     = var.tunnel_secret
-}
-
-output "key" {
-  value     = cloudflare_argo_tunnel.home.tunnel_token
-  sensitive = true
 }
 
 resource "cloudflare_tunnel_config" "tunnel" {
@@ -91,11 +86,11 @@ resource "cloudflare_tunnel_config" "tunnel" {
       for_each = {
         for index, record in var.dns_records :
         record.name => record
-        if record.protected == true && record.local-port != ""
+        if record.zero_trust.protected == true && record.zero_trust.local-port != ""
       }
       content {
         hostname = "${ingress_rule.value.name}.${var.domain.name}"
-        service  = "${ingress_rule.value.protocol}://${ingress_rule.value.local-ip != "" ? ingress_rule.value.local-ip : ingress_rule.value.name}:${ingress_rule.value.local-port}"
+        service  = "${ingress_rule.value.zero_trust.local-protocol}://${ingress_rule.value.zero_trust.local-ip != "" ? ingress_rule.value.zero_trust.local-ip : ingress_rule.value.name}:${ingress_rule.value.zero_trust.local-port}"
       }
     }
     ingress_rule {
@@ -108,7 +103,7 @@ resource "cloudflare_record" "dns-tunnel" {
   for_each = {
     for index, record in var.dns_records :
     record.name => record
-    if record.protected == true && record.protocol != ""
+    if record.zero_trust.tunnel.local-protocol != ""
   }
   zone_id         = data.cloudflare_zone.domain.zone_id
   name            = each.value.name
--- cloudflare-auto/variables.tf
+++ cloudflare-auto/variables.tf
@@ -1,31 +1,69 @@
+variable "default_allowed_idps" {
+  description = "Unless you specify allowed_ips in each `dns_record.zero_trust` this will be used as the default IDPs list"
+  type = string
+  validation {
+    condition = can(regex("^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$", var.default_domain))
+    error_message = "Misconfigured IDPs (Should be a UUID)"
+  }
+  default = null
+}
+
+variable "default_allowed_emails" {
+  description = "Unless you specify dns_record.zero_trust.allowed_mails in each `dns_record` this will be used as the default email list"
+  type = string
+  validation {
+    condition = can(regex("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$", var.default_domain))
+    error_message = "Misconfigured e-mailaddresses!)"
+  }
+  default = null
+}
+
+variable "default_tunnel_name" {
+  description = "Unless you specify dns_record.zero_trust.tunnel.name in each `dns_record.zero_trust` this will be used as the default tunnel name"
+  type = string
+  default = null
+}
+
 variable "dns_records" {
   description = "Value of proxied DNS records"
-  type = list(object({
-    name       = string
-    type       = string
-    value      = string
-    protected  = bool
-    proxied    = bool
-    local-ip   = string
-    local-port = string
-    protocol   = string
-    allowed_idps = list(string)
-    allowed_emails = list(string)
-  }))
-  default = [{ "name" : "", "type" : "", "value" : "", "protected" : false, "proxied" : true, "local-ip": "", "local-port" : "", "protocol" : "", "allowed_emails": [""], "allowed_idps": [""] }]
+  type = object({
+    name       = required(string)
+    type       = optional(string)
+    value      = optional(string)
+    proxied    = optional(bool)
+    zero_trust = optional(object({
+      protected = bool
+      allowed_idps = optional(list(string))
+      allowed_emails = optional(list(string))
+      tunnel = optional(object({
+        name = optional(string)
+        local-ip = optional(string)
+        local-port = optional(string)
+        local-protocol = optional(string)
+      }))
+    }))
+  })
+
+  default = {
+    name = null
+    type = null
+    value = null
+    proxied = true
+    zero_trust = null
+  }
 }
 
 variable "cloudflare_api_token" {
   description = "API token for cloudflare"
   type        = string
-  default     = ""
+  default = null
   sensitive = true
 }
 
 variable "tunnel_secret" {
   description = "API token for cloudflare"
   type        = string
-  default     = ""
+  default = null
   sensitive = true
   validation {
     condition     = length(var.tunnel_secret) == 184
@@ -36,23 +74,22 @@ variable "tunnel_secret" {
 variable "domain" {
   description = "Domain that you want to modify"
   type = object({
-    name   = string
-    target = string
+    name   = required(string)
+    target = optional(string)
+    proxied = bool
+    ttl = number
   })
-  default = {
-    name   = ""
-    target = ""
-  }
-}
-
-variable "allowed_emails" {
-  description = "A list of e-mailaddresses that can login via Cloudflare Access"
-  type        = list(string)
-  default     = [""]
+  default = null
 }
 
 variable "default_ttl" {
   description = "Default TTL"
   type        = number
   default     = 3600
+}
+
+variable "allow_dns_overwrite" {
+  description = "Allows overwrite of DNS"
+  type        = bool
+  default     = false
 }
