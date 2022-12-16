acce0f1fb1b55c3e3ce71b8248931ea16674bf11
Jonas Thim <jonas.thim@gmail.com>
2022-12-16 16:38:27 +0100

    .

--- cloudflare-auto/main.tf
+++ cloudflare-auto/main.tf
@@ -65,7 +65,7 @@ resource "cloudflare_access_policy" "policy" {
 }
 
 resource "cloudflare_argo_tunnel" "default" {
-  for each = {
+  for_each = {
     for index, tunnel in concat(var.default_tunnel.name+var.dns_records[*].zero_trust.tunnel.name) : index => tunnel
   }
   account_id = data.cloudflare_zone.domain.account_id
